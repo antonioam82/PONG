40fda88ade0e4cb1cea56b6c59962e8f12d12ca4
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-12-14 19:34:18 +0100

    Update pong.py

--- pong.py
+++ pong.py
@@ -34,6 +34,14 @@ ball.goto(0, 0)
 ball.dx = 0.2#2
 ball.dy = 0.2#2
 
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0",align="center", font=("Courier", 24, "bold"))
+
 def paddle_a_up():
     y = paddle_a.ycor()
     y += 20
@@ -90,5 +98,5 @@ while True:
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
         ball.setx(-340)
-        ball.dx *= -1
+        ball.dx *= -1 
 
