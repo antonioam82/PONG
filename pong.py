41d80a523ab49944fdb876df741c6eff3408271f
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2020-12-15 16:14:03 +0100

    Update pong.py

--- pong.py
+++ pong.py
@@ -6,6 +6,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+#SCORE
+score_a = 0
+score_b = 0
+
 #PADLE A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -87,10 +91,16 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b),align="center", font=("Courier", 24, "bold"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b),align="center", font=("Courier", 24, "bold"))
         
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
         ball.setx(340)
@@ -98,5 +108,6 @@ while True:
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_a.ycor() - 50):
         ball.setx(-340)
-        ball.dx *= -1 
+        ball.dx *= -1        
+
 
